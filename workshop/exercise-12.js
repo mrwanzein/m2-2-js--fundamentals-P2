62285997bb331b5ba10e0cfa2cfcd85274982683
Mrwan Zein <mrwanzein@outlook.com>
2020-06-16 15:21:32 -0400

    Exercise-12 Answer

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -10,6 +10,26 @@
 
 // Write function here
 
+function calculateAverage(grades) {
+    let sum = 0;
+
+    for(let i=0; i<grades.length; i++){
+        sum += grades[i];
+    }
+
+    let avg = Math.round(sum/grades.length);
+
+    if(avg < 60) return `F`;
+    if(avg >= 60 && avg < 70) return `D`;
+    if(avg >= 70 && avg < 80) return `C`;
+    if(avg >= 80 && avg < 90) return `B`;
+    if(avg >= 90 && avg <= 100) return `A`;
+
+}
+
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
+    console.log(calculateAverage([48, 95, 65, 48, 59, 78, 72, 65]));
 
 // Try with other values as well
+
+console.log(calculateAverage([79, 95, 95, 81, 88, 100, 92, 89]));
