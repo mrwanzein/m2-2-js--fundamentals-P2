7da8a7f9d1772cc9b3861ed4896d332f49a949df
Mrwan Zein <mrwanzein@outlook.com>
2020-06-16 14:53:52 -0400

    Exercise-10 Answer

--- workshop/exercise-10.js
+++ workshop/exercise-10.js
@@ -7,7 +7,12 @@
 // Returns true if every element of lst is of length at least 5.
 // Otherwise returns false.
 function allLong(lst) {
-  // lst is an array of strings
+  if(lst.every(function(str) {
+    return str.length >= 5;
+  })) {
+    return true;
+  }
+  return false;
 }
 // -------------------------------------------------------------------------
 console.log('Q8 - case 1', allLong(['Scott', 'Bob', 'Ric', 'Jim']));
