bb3b55eec17c68ccb39900f52030433498cdfedc
Mrwan Zein <mrwanzein@outlook.com>
2020-06-16 14:07:00 -0400

    Exercise-2 Answer

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,10 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
+function max(num1, num2) {
+    if(num1>num2) console.log(num1);
+    else console.log(num2);
+}
 
 // Call the function
-max(12, 43);
+max(215, 43);
