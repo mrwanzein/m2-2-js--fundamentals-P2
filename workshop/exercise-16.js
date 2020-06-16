a68da6100621b58c96b71b48f8a6ba68d0a0e90f
Mrwan Zein <mrwanzein@outlook.com>
2020-06-16 16:00:59 -0400

    Exercise-16 Answer

--- workshop/exercise-16.js
+++ workshop/exercise-16.js
@@ -8,8 +8,22 @@
 // to the number itself. For example,
 // 371 is an Armstrong number since 3^3 + 7^3 + 1^3 = 371.
 
-let armstrongNumbers = [];
+let armstrongNumbers = function() {
+    let armstrongNumbers = [];
 
-// write your loop here...
+    for(let i=0; i<=999; i++) {
+        let sum = 0;
+        let arrayNums = i.toString().split('');
 
-console.log(armstrongNumbers);
+        for(let j=0; j<arrayNums.length; j++){
+            sum += Math.pow(parseInt(arrayNums[j]), 3);
+        }
+
+        if(sum === parseInt(arrayNums.join(''))) armstrongNumbers.push(sum);
+
+    }
+
+    return armstrongNumbers;
+}
+
+console.log(armstrongNumbers());
