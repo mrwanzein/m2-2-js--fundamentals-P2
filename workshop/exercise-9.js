b837ee8a3db075b6c20a5a69714db14208f58efb
Mrwan Zein <mrwanzein@outlook.com>
2020-06-16 14:48:49 -0400

    Exercise-9 Answer

--- workshop/exercise-9.js
+++ workshop/exercise-9.js
@@ -9,7 +9,11 @@
 // For example: greeLong(["bob", "daniel"]) returns ["Hello daniel"]
 
 function greetLong(lst) {
-  // lst is an array of strings
+  return lst.filter(function(str) {
+    return str.length > 4;
+  }).map(function(str) {
+    return `Hello ${str}`;
+  })
 }
 // -------------------------------------------------------------------------
 console.log('Q7 greetLong()', greetLong(['Scott', 'Bob', 'Ric', 'Jim']));
